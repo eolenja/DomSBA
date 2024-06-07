969a8dd28f536fd8c195960be3ebb6fc5fccb8c5
eolenja <167790428+eolenja@users.noreply.github.com>
2024-06-07 14:49:10 -0400

    debugged form & alert

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 
+const btn = document.createElement("button");
+btn.innerHTML = "top";
+document.body.appendChild(btn);
+
 
 const leftCol = document.getElementById('leftcolumn')
 const yesCount = document.getElementById("yes-count");
@@ -7,24 +11,29 @@ const submitButton = document.getElementById("vote-button")
 let yesVotes = 0;
 let noVotes = 0;
 
-submitButton.addEventListener("click",(e) =>{
-    yesCount.innerHTML = yesVotes
-    noCount.innerHTML= noVotes
+// submitButton.addEventListener("click",(e) => {
+//     yesCount.innerHTML = yesVotes
+//     noCount.innerHTML= noVotes
     
-});
+// });
 
 submitButton.addEventListener('click',(event) => {
     event.preventDefault();
     const selectedVote = document.querySelector('input[name="vote"]:checked');
-    if (selectedVote.value ==='yes') {
-        yesVotes++;
-        yesVotes.textContent = yesVotes;
-    } else if (selectedVote.value === 'no'){
-        noVotes++;
-        noVotes.textContext=noVotes
+   // console.log(selectedVote.value)
+ 
+    try {
+        if (selectedVote.value ==='yes') {
+            yesVotes++;
+            yesCount.textContent = yesVotes;
+        } else {
+            noVotes++;
+            noCount.textContent= noVotes
+        }
+    } catch (error) {
+        alert(error)
     }
-    else { 
-        window.alert('please select an option before submitting');
-    }
+  
+  
     
 }) 
